5f3fad2b2680cafd6c1e98701de33a56e6960437
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-08-11 21:31:33 -0300

    Creating two pages to try out the Link from Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function HomePage() {
 	return (
 		<div>
 			<h1>Alura Cases - Home</h1>
+			<Link href="/faq">
+				<a>Ir para o FAQ</a>
+			</Link>
 		</div>
 	);
 }
